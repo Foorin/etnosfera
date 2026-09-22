cbc989f1e23616c65d8fa0bc92ae435a1e73f5a8
Marina <kvlmarina6@gmail.com>
2026-09-22 13:20:35 +0300

    Настройка сборки для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:8787',
-    },
-  },
+                            server: {
+                              proxy: {
+                                '/api': 'http://127.0.0.1:8787',
+                              },
+                            },
 })
